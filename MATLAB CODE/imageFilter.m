8051e46611b1874f6e770b85d661e1134f101555
muhammadfathurachman <thalhah.muhammad@gmail.com>
2016-10-02 21:57:59 -0700

    Final Image Filtering

--- MATLAB CODE/imageFilter.m
+++ MATLAB CODE/imageFilter.m
@@ -1,4 +1,25 @@
-function imageFiltering = imageFilter(image)
+function imageFiltering = imageFilter(image,filter_size,stride)
+[row column] = size(image);
+ row_iter = 1;
+ col_iter = 1;
+ imageFiltering = {};
+ counter =  1;
+lc = column-filter_size+1;
+lr = row-filter_size+1;
 
+for i = 1 : lr
+    for j = 1 :lc
+       %(col_iter+filter_size-1)
+        imageFiltering{counter} = image(row_iter:(row_iter+filter_size-1),col_iter:(col_iter+filter_size-1));
+        counter = counter + 1;
+        if col_iter + filter_size - 1 <= column
+           col_iter = col_iter + stride;
+        end
+    end
+    if row_iter + filter_size - 1 <= row
+       row_iter = row_iter + stride;
+       col_iter = 1;
+    end
+end
 
 end
